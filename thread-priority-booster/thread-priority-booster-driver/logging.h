15318f60f061c40e2508fd6b762d744d299fd91b
Reversive <matiaspavan@gmail.com>
2023-02-23 12:52:33 -0300

    refac: swapped vDbgPrintEx to vDbgPrintExWithPrefix

--- thread-priority-booster/thread-priority-booster-driver/logging.h
+++ thread-priority-booster/thread-priority-booster-driver/logging.h
@@ -2,6 +2,8 @@
 #include <stdarg.h>
 #include <wdm.h>
 
+#define DRIVER_PREFIX "Booster > "
+
 enum class LogLevel
 {
 	Error = 0,
@@ -18,7 +20,7 @@ NTSTATUS Log(PCSTR format, ...)
 	NTSTATUS status = STATUS_SUCCESS;
 	va_list list;
 	va_start(list, format);
-	status = vDbgPrintEx(DPFLTR_IHVDRIVER_ID, static_cast<ULONG>(LogLevel::Error), format, list);
+	status = vDbgPrintExWithPrefix(DRIVER_PREFIX, DPFLTR_IHVDRIVER_ID, static_cast<ULONG>(LogLevel::Error), format, list);
 	va_end(list);
 	return status;
 }
